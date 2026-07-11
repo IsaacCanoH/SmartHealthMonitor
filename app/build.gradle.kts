f84988e26e0210e388cb7d4c2428475200ce0f4a
IsaacCanoH <isac.c.2586@gmail.com>
2026-07-10 18:37:48 -0600

    feat(cast): add Cast SDK dependencies to app module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
 
     // Necesario para generar el código del DAO
     ksp("androidx.room:room-compiler:$roomVersion")
+
+    // Cast SDK
+    implementation("androidx.mediarouter:mediarouter:1.7.0")
+    implementation("com.google.android.gms:play-services-cast-framework:21.5.0")
 }
